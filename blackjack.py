75a6594cd063edb9b43943e001c8726cf42c2f9e
Michael Huitt <michael.c.huitt@gmail.com>
2025-08-28 23:17:58 -0400

    init commit

---
+++ blackjack.py
@@ -0,0 +1,57 @@
+import random ##for shuffle
+from os import system ##for screen clearing
+
+SEPERATOR_LEN = 30
+
+balance = 1000
+wager = 15 ##default, can be changed in main game loop
+
+def display_hand(hand):
+    for card in hand:
+        if card == hand[-1]:
+            print(card)
+        else:
+            print(card, end = ", ")
+
+def display_header():
+    print('=' * SEPERATOR_LEN)
+    print(f"BALANCE: {balance}\nWAGER: {wager}")
+    print('-' * SEPERATOR_LEN)
+
+def main():
+    player_hand = []
+    dealer_hand = []
+    deck = [] 
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"] 
+        
+    for suit in suits:
+        for rank in ranks:
+            deck.append(f"{rank} of {suit}")
+        
+    random.shuffle(deck)
+       
+    system("clear || cls") 
+    display_header()
+
+    print("PLAY? (y/n)")
+    yn_prompt = input()
+        
+    if yn_prompt.lower() == "y":
+        for draw in range(2):
+            dealer_hand.append(deck.pop())
+            player_hand.append(deck.pop())
+            
+        player_score = dealer_score = 0
+        
+        system("clear || cls")
+        display_header()
+        print(f"DEALER HAND: {dealer_hand[0]}\n\nPLAYER HAND:")
+        display_hand(player_hand)
+
+        input()
+        
+    elif yn_prompt.lower() == "n":
+        return
+
+main()
